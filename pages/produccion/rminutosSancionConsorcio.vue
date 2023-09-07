43a91bf119f5c585d7e10df81a9490dba9e52d18
Payrouse <leonardobone99@gmail.com>
2023-09-07 10:14:46 -0500

    update

--- pages/produccion/rminutosSancionConsorcio.vue
+++ pages/produccion/rminutosSancionConsorcio.vue
@@ -141,6 +141,9 @@
 <script>
 import flatPicker from "vue-flatpickr-component";
 import "flatpickr/dist/flatpickr.css";
+import pdfMake from "pdfmake/build/pdfmake.js";
+import pdfFonts from "pdfmake/build/vfs_fonts.js";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 import {
   Table,
   TableColumn,
